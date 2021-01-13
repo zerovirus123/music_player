c6b1c1086987fb82c50c08189ce63f465e3c7274
zerovirus123 <briansia93@gmail.com>
2021-01-13 11:35:56 +0700

    added React lifecycle hooks to automatically redirect users to their rooms

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CreateRoomPage from './CreateRoomPage';
 import RoomJoinPage from './RoomJoinPage';
 import Room from './Room';
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
 import { BrowserRouter as Router, 
     Switch, 
     Route, 
@@ -11,15 +12,53 @@ import { BrowserRouter as Router,
 export default class HomePage extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            roomCode: null,
+        }
+    }
+
+    //lifecycle methods to hook into the React methods
+    //async allows the other parts of the program to execute when this function runs
+    async componentDidMount(){
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code
+            })
+        });
+    }
+
+    renderHomePage(){
+        return(
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                 <Typography variant="h3" compact="h3">
+                    House Party
+                 </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
     }
 
     render(){
         return(
             <Router>
                 <Switch>
-                    <Route exact path="/">
-                        <p>This is the homepage.</p>
-                    </Route>
+                    <Route exact path="/" render={() => {
+                        return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}></Redirect>) : this.renderHomePage
+                    }}/>
+
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
                     <Route path="/room/:roomCode" component={Room} />
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
-
+import { TextField, Button, Grid, Typography, Text } from "@material-ui/core"
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component{
-
     constructor(props){
         super(props);
+        this.state = {
+            roomCode: "",
+            error: ""
+        }
+
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this)
+        this.roomButtonPressed = this.roomButtonPressed.bind(this)
     }
 
     render(){
-        return <p> This is the join room page </p>;
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                    Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField 
+                     error={this.state.error} 
+                     label="Code" 
+                     placeholder="Enter a Room Code"
+                     value={this.state.roomCode}
+                     helperText={this.state.Error}
+                     variant="outlined"
+                     onChange={this.handleTextFieldChange}
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" 
+                            color="primary" 
+                            onClick={this.roomButtonPressed}>
+                                Enter Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" 
+                            color="secondary" 
+                            to="/" 
+                            component={Link}>
+                                Back
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    handleTextFieldChange(e){
+        this.setState({
+            roomCode: e.target.value
+        })
+    }
+
+    roomButtonPressed(){
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                code: this.state.roomCode
+            })
+        };
+        fetch('/api/join-room', requestOptions)
+        .then((response) => {
+             if (response.ok){
+                this.props.history.push(`/room/${this.state.roomCode}`); //backticks allow string formatting
+             } else {
+                this.setState({ Error: "Room not found." })
+             }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 }
