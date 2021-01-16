28827dea237121e53714bb8e355b3ea83edf66f0
zerovirus123 <briansia93@gmail.com>
2021-01-16 14:38:47 +0700

    added Leave room feature

--- api/views.py
+++ api/views.py
@@ -81,7 +81,6 @@ class JoinRoom(APIView):
         return Response({'Bad Request': 'Invalid post data did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserInRoom(APIView):
-
     def get(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
@@ -91,3 +90,16 @@ class UserInRoom(APIView):
         }
 
         return JsonResponse(data, status=status.HTTP_200_OK)
+
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+
+        return Response({"Message": "Success"}, status=status.HTTP_200_OK)
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,70 +1,88 @@
-import React, { Component } from 'react';
-import CreateRoomPage from './CreateRoomPage';
-import RoomJoinPage from './RoomJoinPage';
-import Room from './Room';
-import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
-import { BrowserRouter as Router, 
-    Switch, 
-    Route, 
-    Link,
-    Redirect } from "react-router-dom"
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-export default class HomePage extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            roomCode: null,
-        }
-    }
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: null,
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
+  }
 
-    //lifecycle methods to hook into the React methods
-    //async allows the other parts of the program to execute when this function runs
-    async componentDidMount(){
-        fetch('/api/user-in-room')
-        .then((response) => response.json())
-        .then((data) => {
-            this.setState({
-                roomCode: data.code
-            })
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
         });
-    }
+      });
+  }
 
-    renderHomePage(){
-        return(
-            <Grid container spacing={3}>
-                <Grid item xs={12} align="center">
-                 <Typography variant="h3" compact="h3">
-                    House Party
-                 </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <ButtonGroup disableElevation variant="contained" color="primary">
-                        <Button color="primary" to="/join" component={ Link }>
-                            Join a Room
-                        </Button>
-                        <Button color="secondary" to="/create" component={ Link }>
-                            Create a Room
-                        </Button>
-                    </ButtonGroup>
-                </Grid>
-            </Grid>
-        );
-    }
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
 
-    render(){
-        return(
-            <Router>
-                <Switch>
-                    <Route exact path="/" render={() => {
-                        return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}></Redirect>) : this.renderHomePage
-                    }}/>
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
 
-                    <Route path="/join" component={RoomJoinPage} />
-                    <Route path="/create" component={CreateRoomPage} />
-                    <Route path="/room/:roomCode" component={Room} />
-                </Switch>
-            </Router>
-        );
-    }
-
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
