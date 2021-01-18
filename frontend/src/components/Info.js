9362371b36a44ad3224a269a234fb224a0cf88b9
zerovirus123 <briansia93@gmail.com>
2021-01-18 11:52:24 +0700

    added Create/Join instructions in the Info page

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -9,16 +9,40 @@ const pages = {
     CREATE: 'pages.create',
 };
 
-
 export default function Info(props){
     const [page, setPage] = useState(pages.JOIN);
 
     function joinInfo(){
-        return "Join Page";
+
+        return(
+            <Grid item xs={12} align="center">
+            <Typography component="h6" variant="h6">
+                Join Page
+            </Typography>
+            <Typography component="h7" variant="h7" align="center">
+            Join a room by clicking on Join Room, and then key in the 6 letter passcode. 
+            Inside the room, users can vote to skip songs if the room creator allows it.
+            </Typography>
+        </Grid>
+        );
     }
 
     function createInfo(){
-        return "Create Page";
+
+        return(
+            <Grid item xs={12} align="center">
+            <Typography component="h6" variant="h6">
+                Create Page
+            </Typography>
+            <Typography component="h7" variant="h7" align="center">
+            Click Create Room in the homepage to create a room. 
+            The host can set the playback state: play/pause enabled or no control. 
+            In addition, the user can set the number of votes required to skip a song. 
+            Once a room is create, a 6 letter uppercase passcode is generated.
+            Other people can join the room by keying in the passcode.
+            </Typography>
+        </Grid>
+        );
     }
 
     //this hook runs the function inside whenever a component lifcycle method is called
@@ -30,7 +54,7 @@ export default function Info(props){
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-            <Typography component="h4" variant="h4">
+            <Typography component="h3" variant="h3">
                 What is House Party?
             </Typography>
         </Grid>
